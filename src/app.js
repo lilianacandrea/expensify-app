b7476ea24134562fb2409e5c2846d197aae512c1
lilianacandrea <gogonicipici@gmail.com>
2018-08-03 15:12:03 +0300

    Linking between routes. We can swetch pages without going through the full page refresh. Styling the current NavLink to be bold.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -29,10 +29,22 @@ const HelpPage = () => (
     </div>
 );
 
+// Linking between routes : add an event listener for our links, then we're going to override the brower's default behavior
+// <a href="/">Go Home</a>
 const NotFoundPage = () => (
     <div>
-        404
+        404 - <Link to="/">Go home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Espense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 // Set a route with 2 props.
@@ -41,13 +53,17 @@ const NotFoundPage = () => (
 // exact: it changes how matching works.
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpPage}/>
+                <Route component={NotFoundPage} />
+            
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
